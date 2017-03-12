aa45b4cf25f24243668cb63521c67d918588a045
Rob Garrison <wowmotty@gmail.com>
2017-03-11 20:58:54 -0600

    Move node require to start

--- index.js
+++ index.js
@@ -7,19 +7,10 @@
 "use strict";
 
 const Util = require("./src/util");
-
-/**
- * A user would need to create a new instance
- * @example
- * const transliterate = require("diacritics-transliterator").transliterate,
- *     instance = new transliterate();
- * console.log(instance.transliterate("some string with diacritics"));
- */
 module.exports = require("./src/diacritics");
 module.exports.getData = require("./src/cache").getData;
-module.exports.getDiacritics = Util.getDiacritics;
-module.exports.formatUnicode = Util.formatUnicode;
 module.exports.transliterate = require("./src/transliterate").transliterate;
 module.exports.createRegExp = require("./src/regexp").createRegExp;
-module.exports.replacePlaceholder =
-    require("./src/placeholder").replacePlaceholder;
+module.exports.replacePlaceholder = require("./src/placeholder").replacePlaceholder;
+module.exports.getDiacritics = Util.getDiacritics;
+module.exports.formatUnicode = Util.formatUnicode;
